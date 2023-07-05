5de3406978b5d8d27e5af85b1858c61a87904ed9
prathamdey <deypratham04@gmail.com>
2023-07-05 17:44:42 +0530

    commit to share repositoy

---
+++ providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = "~> 1.2.2"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.66"
+      #version = "~> 4.19.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region     = var.region
+}
